da7502946c7fdc7dee819ad0d5d6aa4da3c3574d
franklin16 <franklinabao@gmail.com>
2015-08-22 12:43:45 +1000

    p65-66

--- 09-app/server.js
+++ 09-app/server.js
@@ -45,6 +45,16 @@ app.get('/', function(req, res) {
 // get an instance of the express router
 var apiRouter = express.Router();
 
+//middleware to use for all requests
+apitRouter.use(function(req, res, next) {
+	//do logigng
+	console.log('Somebody just came to our app!');
+	//we'll add more to the middleware in Chapter 10
+	//this is where we will authenticate users
+
+	next(); //make sure we go to the next routes and don't stop here
+})
+
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
 
 
