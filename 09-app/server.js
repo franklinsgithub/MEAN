0810f28a38132f937aad39a6ce26c41d3a4b7e52
franklin16 <franklinabao@gmail.com>
2015-08-22 15:46:29 +1000

    ch 09 and 10

--- 09-app/server.js
+++ 09-app/server.js
@@ -42,6 +42,7 @@ app.get('/', function(req, res) {
 // get an instance of the express router
 var apiRouter = express.Router();
 
+
 //middleware to use for all requests
 apiRouter.use(function(req, res, next) {
 	//do logigng
@@ -51,8 +52,6 @@ apiRouter.use(function(req, res, next) {
 
 	next(); //make sure we go to the next routes and don't stop here
 })
-
-// route to authenticate a user (POST http://localhost:8080/api/authenticate)
 
 
 // test route to make sure everything is working
@@ -83,8 +82,69 @@ apiRouter.route('/users')
 					return res.send(err);
 			}
 			res.json({ message: 'User created!' });
-			});
+		});
 	})
+	// get all users (accessed at GET http://localhost:8080/api/users)
+	.get(function(req, res) {
+		User.find(function(err, users) {
+			if (err) res.send(err);
+
+			// return the users
+			res.json(users);
+		});
+	});
+
+// on routes that end in /users/:user_id
+// =======================================
+apiRouter.route('/users/:user_id')
+
+	// get the user with that id
+	// (accessed at GET http://localhost:8080/api/users/:user_id)
+	.get(function(req, res) {
+		User.findById(req.params.user_id, function(err, user) {
+			if (err) res.send(err);
+
+			// return that user
+			res.json(user);
+		});
+	})
+
+	// update the user with this id
+	// (accessed at PUT http://localhost:8080/api/users/:user_id)
+	.put(function(req, res) {
+
+		// use our user model to find the user we want
+		User.findById(req.params.user_id, function(err, user) {
+
+			if (err) res.send(err);
+
+			// update the users info only if its new
+			if (req.body.name) user.name = req.body.name;
+			if (req.body.username) user.username = req.body.username;
+			if (req.body.password) user.password = req.body.password;
+
+			// save the user
+			user.save(function(err) {
+				if (err) res.send(err);
+
+				// return a message
+				res.json({ message: 'User updated!' });
+			});
+		});
+	})
+
+	// delete the user with this id
+	// (accessed at DELETE http://localhost:8080/api/users/:user_id)
+	.delete(function(req, res) {
+		User.remove({
+			_id: req.params.user_id
+		}, function(err, user) {
+			if (err) return res.send(err);
+
+			res.json({ message: 'Successfully deleted' });
+		});
+	});
+
 
 // REGISTER OUR ROUTES
 // all of our routes will be prefixed with /api
